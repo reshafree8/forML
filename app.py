0b2f4aed99e8b9365b7ec0f8bef48cebb77c2c9e
afreen sultana <nayeemataj024@gmail.com>
2025-04-05 00:52:18 +0530

    for port

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ nltk.download('wordnet')
 
 from nltk.corpus import stopwords
 # rest of your imports...
+import os
 
 from flask import Flask, request, render_template
 import joblib
@@ -41,4 +42,6 @@ def predict():
     return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
